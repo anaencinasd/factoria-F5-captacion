21516f025120af21454a9a2fc038e21461a2c87c
anaencinasd <ana.encinasdiaz@gmail.com>
2023-09-26 09:41:19 +0200

    models, controllers, migrations done

---
+++ factoria-f5-captacion/app/Http/Controllers/PersonRequirementController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Person_Requirement;
+use Illuminate\Http\Request;
+
+class PersonRequirementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Person_Requirement $person_Requirement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Person_Requirement $person_Requirement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Person_Requirement $person_Requirement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Person_Requirement $person_Requirement)
+    {
+        //
+    }
+}
---
+++ factoria-f5-captacion/app/Http/Controllers/PersonSelectionDayController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Person_SelectionDay;
+use Illuminate\Http\Request;
+
+class PersonSelectionDayController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Person_SelectionDay $person_SelectionDay)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Person_SelectionDay $person_SelectionDay)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Person_SelectionDay $person_SelectionDay)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Person_SelectionDay $person_SelectionDay)
+    {
+        //
+    }
+}
