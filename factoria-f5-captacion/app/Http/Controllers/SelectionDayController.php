f4ba4e7fb30307f7823a94290f0d9aaa38679243
LilinaG <lgutierrezfer@gmail.com>
2023-09-26 12:40:37 +0200

    SelectionDay Controller done

---
+++ factoria-f5-captacion/app/Http/Controllers/SelectionDayController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SelectionDay;
+use Illuminate\Http\Request;
+use App\Http\Requests\SelectionDayRequest;
+use Illuminate\Http\JsonResponse;
+
+class SelectionDayController extends Controller
+{
+    
+    public function index():JsonResponse
+    {
+        $selectionDays = SelectionDay::all();
+        return response()->json(['data'=>$selectionDays], 200);
+     
+    }
+
+   
+    public function store(SelectionDayRequest $request):JsonResponse
+    {
+        $selectionDay=SelectionDay::create($request->all());
+
+        return response()->json([
+            'success'=>true,
+            'data'=>$selectionDay], 201);
+        
+    }
+
+    
+    public function show($id):JsonResponse
+    {
+        $selectionDay = SelectionDay::find($id);
+        return response ()->json($selectionDay, 200);
+    }
+
+    
+    public function update(SelectionDayRequest $request, $id):JsonResponse
+    {
+        $selectionDay = SelectionDay::find($id);
+        $selectionDay->school=$request->school;
+        $selectionDay->date=$request->date;
+        $selectionDay->save();
+
+        return response()->json([
+            'success'=>true
+        ], 200);
+    }
+
+    
+    public function destroy($id):JsonResponse
+    {
+        SelectionDay::find($id)->delete();
+        return response()->json([
+            'success'=>true
+        ], 200);
+    }
+}
+
